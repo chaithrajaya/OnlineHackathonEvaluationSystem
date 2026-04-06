403b9cb2d8819b922e0a72f8ea8b7686e1329b28
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2026-04-06 13:09:17 +0530

    Add files via upload

---
+++ HackathonEvaluator/src/main/java/com/example/HackathonEvaluator/HackathonEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackathonEvaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonEvaluatorApplication.class, args);
+	}
+
+}
